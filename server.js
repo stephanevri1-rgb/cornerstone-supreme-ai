3b93556e74132aae2c615f760f72b0fffbc67202
stephanevri1-rgb <stephanevri1@gmail.com>
2026-07-01 01:51:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-app.use(cors());
+app.use(cors()); 
 app.use(express.json({ limit: '50mb' }));
 
 // ENVIRONMENT VARIABLES
